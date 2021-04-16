f3bf80a85fa7517cfe3e5f12ec165e45b16449e5
HJW <huangjingwei@goluk.com>
2021-04-17 01:19:53 +0800

    首页TAB切换UI

---
+++ app/src/main/java/com/metron/coin/ui/home/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.metron.coin.ui.home;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.metron.coin.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.tab_home, R.id.tab_profit, R.id.tab_minter, R.id.tab_mine)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_home_tab);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
---
+++ app/src/main/java/com/metron/coin/ui/home/MineFragment.java
@@ -0,0 +1,25 @@
+package com.metron.coin.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.metron.coin.R;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MineFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text);
+        textView.setText("我的");
+
+        return root;
+    }
+}
