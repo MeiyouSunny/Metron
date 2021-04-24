1a0007e180abe7cd7f4aff2ba4486c6557334b95
HJW <huangjingwei@goluk.com>
2021-04-24 21:56:32 +0800

    矿机类型页面

--- app/src/main/java/com/metron/coin/ui/home/MinterFragment.java
+++ app/src/main/java/com/metron/coin/ui/home/MinterFragment.java
@@ -1,22 +1,54 @@
 package com.metron.coin.ui.home;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.metron.coin.R;
+import com.metron.coin.base.BaseBindFragment;
+import com.metron.coin.databinding.FragmentMinterBinding;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+public class MinterFragment extends BaseBindFragment<FragmentMinterBinding> {
 
-public class MinterFragment extends Fragment {
+    private final int[] mIcons = {R.drawable.ic_btc, R.drawable.ic_btc, R.drawable.ic_eth, R.drawable.ic_usdt};
+    private final int[] mTitles = {R.string.miner_all, R.string.miner_btc, R.string.miner_eth, R.string.miner_usdt};
 
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-        return root;
+    @Override
+    public int initLayoutResId() {
+        return R.layout.fragment_minter;
     }
+
+    @Override
+    public void onViewCreated() {
+        super.onViewCreated();
+
+        bindRoot.setIcons(mIcons);
+        bindRoot.setTitles(mTitles);
+        bindRoot.setType(0);
+    }
+
+    @Override
+    public void click(View view) {
+        switch (view.getId()) {
+            case R.id.typeSelect:
+                bindRoot.setTypeListVisible(!bindRoot.getTypeListVisible());
+                break;
+            case R.id.typeAll:
+                selectType(0);
+                break;
+            case R.id.typeBTC:
+                selectType(1);
+                break;
+            case R.id.typeETH:
+                selectType(2);
+                break;
+            case R.id.typeUSDT:
+                selectType(3);
+                break;
+        }
+    }
+
+    private void selectType(int type) {
+        bindRoot.setTypeListVisible(false);
+        bindRoot.setType(type);
+    }
+
 }
