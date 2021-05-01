ef53b4a33c35770c630f4e5526a8e91a101700fc
wangbing01 <bing.wang@gwm.cn>
2021-05-01 23:00:27 +0800

    用户协议/隐私政策

--- app/src/main/java/com/metron/coin/ui/settings/AboutActivity.java
+++ app/src/main/java/com/metron/coin/ui/settings/AboutActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import com.metron.coin.R;
 import com.metron.coin.base.BaseTitleActivity;
 import com.metron.coin.databinding.ActivityAboutBinding;
+import com.metron.coin.ui.webpage.WebPageActivity;
 
 /**
  * 关于
@@ -29,9 +30,13 @@ public class AboutActivity extends BaseTitleActivity<ActivityAboutBinding> {
     @Override
     public void click(View view) {
         switch (view.getId()) {
-//            case R.id.exit:
-//                exitAccount();
-//                break;
+            // TODO
+            case R.id.userAgreement:
+                WebPageActivity.start(this, "https://app.tokensky.cn/articles/zh_CN/agreement-zh_CN.html", "用户协议");
+                break;
+            case R.id.privacyPolicy:
+                WebPageActivity.start(this, "https://app.tokensky.cn/articles/zh_CN/agreement-zh_CN.html", "隐私政策");
+                break;
         }
     }
 
